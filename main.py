dee3697d68c8771217a271961ec34a6973f61de1
Rafaelnotty <113991887+rafaelnotty@users.noreply.github.com>
2023-05-13 15:51:50 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request,make_response,redirect,render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+#Variables de contexto
+todos = ['Quien Soy','Formación','Experiencia','Contacto']
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html',error=error)
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip',user_ip)
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+    
+    return render_template('hello.html',**context)
